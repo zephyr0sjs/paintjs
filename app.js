9f565a10f789ab26ba23697d3d43305633fa43a0
zephyr0s <zephyr0s.js@gmail.com>
2022-01-05 00:01:43 +0900

    lec 2.5

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
-const colors = document.getElementsByClassName("jsColor");
+const colors = document.getElementsByClassName("controls_colors");
+const range = document.getElementById("jsRange");
+const modeBtn = document.getElementById("jsMode");
+
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
 
 // 캔버스 사이즈 정의
-canvas.width = 700;
-canvas.height = 700;
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
 
-ctx.strokeStyle = "#2c2c2c";
-ctx.lineWidth = 2.5;
+ctx.strokeStyle = INITIAL_COLOR; // 브러쉬 색상 값
+ctx.fillStyle = INITIAL_COLOR;
+ctx.lineWidth = 2.5; // 브러쉬 라인 너비
 
-let painting = false;
+let painting = false; // 그림을 그리고 있는 중인지를 판별하는 변수
+let filling = false; // fill 버튼을 클릭했는지 확인하는 변수
 
 function stopPainting() {
   painting = false;
@@ -32,8 +39,30 @@ function onMouseMove(event) {
 }
 
 function handleColorClick(event) {
-  const color = event.target.style.backgroundColor;
-  ctx.strokeStyle = color;
+  const selColor = event.target.style.backgroundColor;
+  ctx.strokeStyle = selColor;
+  ctx.fillStyle = selColor;
+}
+
+function handleRangeChange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+
+function handleModeClick() {
+  if (filling === true) {
+    filling = false;
+    modeBtn.innerText = "Fill";
+  } else {
+    filling = true;
+    modeBtn.innerText = "Paint";
+  }
+}
+
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
 }
 
 if (canvas) {
@@ -41,8 +70,17 @@ if (canvas) {
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
 }
 
 Array.from(colors).forEach((color) =>
   color.addEventListener("click", handleColorClick)
 );
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+}
+
+if (modeBtn) {
+  modeBtn.addEventListener("click", handleModeClick);
+}
